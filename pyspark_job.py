5924a48f95d81575c5bb695bfe38d9cf8565604e
Muhammad Ihsan <muhihsan0@outlook.com>
2024-11-22 13:49:49 +0700

    gcp environment

--- pyspark_job.py
+++ pyspark_job.py
@@ -4,7 +4,7 @@ from helper.pyspark_helper import changeCurrency_udf
 
 spark = SparkSession.builder.appName('amazon sales project').getOrCreate() #Cari tau ini apa/ngaipain
 
-file_path = 'assets/amazon_sales.csv'
+file_path = 'gs://assets_pyspark_project_icano/amazon_sales.csv'
 df = spark.read.csv(file_path, header=True, inferSchema=True)
 
 df = df.withColumn("Date", to_date("Date", "MM-dd-yy"))
@@ -17,4 +17,8 @@ df = df.withColumn("Amount_in_usd", changeCurrency_udf(df["Amount"], df["currenc
 
 df = df.drop("Unnamed: 22")
 
-df.show()
+project_id = "pyspark-icano-project"
+dataset_id = "sales"
+table_id = "amazon_sales_cleansed"
+
+df.write.format("bigquery").option("table", f"{project_id}:{dataset_id}:{table_id}").option("partitonField", "Date").save()
